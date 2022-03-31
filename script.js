ac88e6238dc73bc9b643ced9d3dc77f609106002
Sam Wallace <sam@sammymayhem.com>
2022-03-30 20:12:58 -0700

    Got function to recognize number of characters needed for password.

--- script.js
+++ script.js
@@ -1,9 +1,34 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var userChoice = {
+  alphaLower: "abcdefghijklmnopqrstuvwxyz",
+  alphaUpper: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+  numbers: "0123456789",
+  symbols: "!@#$%^&*_-+=",
+}
+
 function generatePassword() {
   var password = "password";
   // TODO: add code to generate the password here
+
+  // Length of password
+  var userLength = prompt("How many characters would you like your password to be?")
+
+  if (userLength < 8 || userLength > 128) {
+    (password.length = userLength);
+  } else {
+    return;
+  }
+
+  // Want to use lowercase letters?
+
+  var userLower = Math.floor(Math.random() * userChoice.alphaLower.length);
+
+  if ()
+  
+
+
 
   return password;
 }
