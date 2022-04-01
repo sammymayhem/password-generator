2524d236b552b91517e88b20cb99f688a14f037f
Sam Wallace <sam@sammymayhem.com>
2022-03-31 17:40:05 -0700

    userLength will update length. Lowercase if statement works.

--- script.js
+++ script.js
@@ -1,27 +1,45 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-var userChoice = {
-  alphaLower: "abcdefghijklmnopqrstuvwxyz",
-  alphaUpper: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
-  numbers: "0123456789",
-  symbols: "!@#$%^&*_-+=",
-}
+var password = " ";
+var alphaLower = "abcdefghijklmnopqrstuvwxyz";
+var alphaUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numbers = "0123456789";
+var symbols = "!@#$%^&*_-+=";
+var candidates = " ";
+var length = 0;
+var includeLower = false;
+var includeUpper = false;
+var includeSpecial = false;
+var includeNumber = false;
+var letter = " ";
+
 
 function generatePassword() {
   var password = "password";
   // TODO: add code to generate the password here
 
   // Length of password
-  var userLength = prompt("How many characters would you like your password to be?")
+  userLength = prompt("How many characters would you like your password to be?")
 
   if (userLength < 8 || userLength > 128) {
     alert("Password must be between 8 and 128 characters");
-    return "";
-  } 
- 
+    return " ";
+  } else {
+    length = userLength;
+    console.log(length);
+  }
+
+
   // Want to use lowercase letters?
-  
+  var includeLower = confirm("Would you like to use lower case letters?")
+
+  if (includeLower === true) {
+    candidates = alphaLower;
+    console.log(candidates);
+  }
+
+
 
   return password;
 }
@@ -37,3 +55,5 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
